7cf8d18a572cff98c52b6565cc8c78dc5f91f3d0
Devin Edwards <edwards.devin50@gmail.com>
2023-06-26 15:55:12 -0400

    added some ui features

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,8 @@
-import './App.css'
-import { useState } from 'react';
+import './App.css';
+import Home from './scenes/Home/Home';
 
 function App() {
-  const [users, setUsers] = useState([''])
-
-  const getUsers = async () => {
-    await fetch("http://localhost:8080/users")
-      .then(res => res.json())
-      .then(data => data.map((user: { email: string }) => 
-        user.email
-      ))
-      .then(userList => setUsers(userList))
-      .catch(err => {
-        window.alert(err);
-        return;
-      })
-  }
-
-  return (
-    <>
-      <h1>eNuts</h1>
-      <div className="card">
-        <button onClick={() => getUsers()}>
-            Get Users
-        </button>
-        {users.map(user => <p>{user}</p>)}
-      </div>
-    </>
-  )
+  return <Home />
 }
 
 export default App
