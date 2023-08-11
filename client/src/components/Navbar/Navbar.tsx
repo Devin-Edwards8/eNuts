72c2dc0c6724050dc67f844307967ba686bb1c7a
Devin Edwards <edwards.devin50@gmail.com>
2023-08-11 13:37:57 -0400

    adding buttons for account and cart information

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,9 +2,25 @@ import "./Navbar.css"
 import { Link } from "react-router-dom";
 import cartIcon from "../../assets/empty-cart-icon.png"
 import accountIcon from "../../assets/account-icon.png"
+import { useState } from "react";
 
 function Navbar(props: {location: string}) {
   const paths = getPaths(props.location)
+  const [cartPopup, setCartPopup] = useState<"hidden" | "visible">("hidden");
+  const [loginPopup, setLoginPopup] = useState<"hidden" | "visible">("hidden");
+  const [accountPopup, setAccountPopup] = useState<"hidden" | "visible">("hidden");
+  const cartNum = 0;
+  let cartNumDisplayed = "";
+  let cartNumVisibility: "visible" | "hidden" = "hidden";
+  if(cartNum == 0) {
+    cartNumVisibility = "hidden";
+  } else if (cartNum > 0 && cartNum < 100) {
+    cartNumVisibility= "visible";
+    cartNumDisplayed = String(cartNum);
+  } else {
+    cartNumVisibility= "visible";
+    cartNumDisplayed = "+";
+  }
 
   return (
     <div className="header">
@@ -14,8 +30,19 @@ function Navbar(props: {location: string}) {
         <Link to={paths.mapPath}>Locations</Link>
         <Link to={paths.aboutPath}>About Us</Link>
         <div className="icons">
-          <img src={cartIcon} alt="view cart button"/>
-          <img src={accountIcon} alt="view account information button"/>
+          <img src={cartIcon} alt="view cart button" onClick={() => setCartPopup("visible")}/>
+          <span className="cart-number" style={{visibility: cartNumVisibility}}>{cartNumDisplayed}</span>
+          <div style={{visibility: cartPopup}} className="cart-popup">
+            <span className="empty-cart-message">Your cart is empty.</span>
+            <div className="rule" />
+          </div>
+          <img src={accountIcon} alt="view account information button" onClick={() => setLoginPopup("visible")}/>
+          <div style={{visibility: loginPopup}} className="login-popup">
+            <span className="login-text">log in / sign up</span><span>→</span>
+          </div>
+          <div style={{visibility: accountPopup}} className="account-popup">
+
+          </div>
         </div>
       </nav>
     </div>
