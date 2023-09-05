a2b765df96fae296c9f48097866a2ea881e312f7
DevinEdwards <98787106+siiiiw@users.noreply.github.com>
2023-09-04 21:30:20 -0400

    added map with google maps api

--- client/src/scenes/Map/Map.tsx
+++ client/src/scenes/Map/Map.tsx
@@ -1,11 +1,23 @@
-import Navbar from "../../components/Navbar/Navbar"
+import Navbar from "../../components/Navbar/Navbar";
+import "./Map.css";
 
 function Map() {
   return (
     <div className="map">
       <Navbar location="map" />
-      <div>
-        <h1>Map</h1>
+      <h1 id="map-title">Find A Store</h1>
+      <div id="map-area">
+        <div className="search-area">
+          <p>search bar for stores</p>
+          <p>locations appear hear</p>
+        </div>
+        <iframe
+          width="75%"
+          height="100%"
+          className="google-map"
+          loading="lazy"
+          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ3xY3JCdjUjoRYRjM7iwVOXY&key=AIzaSyBTF6K9I4h2Vx7xPiC2q_hhsIHOcyRClPc"
+        ></iframe>
       </div>
     </div>
   );
