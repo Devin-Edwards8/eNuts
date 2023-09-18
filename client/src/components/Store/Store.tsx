13cf729762f7e03930a375f951018950ec4332d2
DevinEdwards <98787106+siiiiw@users.noreply.github.com>
2023-09-18 19:39:54 -0400

    added dynamic map loading

--- client/src/components/Store/Store.tsx
+++ client/src/components/Store/Store.tsx
@@ -1,11 +1,28 @@
 import "./Store.css";
 
-function Store(props: {storeName: string, storeAddress: string}) {
+type Location = {
+  name: string,
+  address: string,
+  storeLink: string,
+  mapsLink: string
+}
+
+function Store(props: {locationInfo: Location, setMap: (s: string) => void, setMapVisibility: (b: number) => void}) {
+  const loc = props.locationInfo;
+  const handleClick = () => {
+    props.setMap(loc.mapsLink);
+    props.setMapVisibility(3);
+  }
   return (  
-    <div className="store-preview">
-      <h1>{props.storeName}</h1>
-      <p>{props.storeAddress}</p>
-      <button>LAUNCH STORE PAGE</button>
+    <div className="store-preview" onClick={() => handleClick()}>
+      <h1>{loc.name}</h1>
+      <p>{loc.address}</p>
+      {
+        loc.storeLink === "" ? <></> :
+          <a href={loc.storeLink} rel="noreferrer" target="_blank">
+            <button>LAUNCH STORE PAGE</button>
+          </a>
+      }
       <div className="open-tag">
         <p>Open</p>
       </div>
