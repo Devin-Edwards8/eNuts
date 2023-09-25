23ac438742ee7f566095208b1b0e0ed1dae3504a
DevinEdwards <98787106+siiiiw@users.noreply.github.com>
2023-09-25 16:59:53 -0400

    tweaks

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -16,7 +16,7 @@ module.exports = {
       ref: 'origin/main',
       repo: 'https://github.com/Devin-Edwards8/eNuts.git',
       path: '/home/ubuntu/eNuts',
-      'post-deploy': 'cd eNuts/current && npm install && pm2 startOrRestart ecosystem.config.cjs && pm2 save'
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.cjs && pm2 save'
     }
   }
 }
